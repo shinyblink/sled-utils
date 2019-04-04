adcbd4d457803347759bff61d890a3768d717c2e
cyriax <brast@cs.uni-frankfurt.de>
2019-04-05 01:07:34 +0200

    svg-util: mockup compiling

--- svg-util/stub.c
+++ svg-util/stub.c
@@ -1,13 +1,20 @@
 
+// MOCKUP STUFF BEGIN
+int matrix_getx(){return 0;}
+int matrix_gety(){return 0;}
+
+typedef int RGB;
+// MOCKUP STUFF END
 
 
+// DATA STUFF BEGIN
 struct point {
     float x;
     float y;
 } point;
 
 struct polygon{
-    point * points;
+    struct point * points;
     int numpoints;
     RGB color;
     float min_x;
@@ -16,7 +23,7 @@ struct polygon{
     float max_y;
 } polygon;
 
-polygon * polys;
+struct polygon * polys;
 int numpolys;
 
 struct line {
@@ -25,9 +32,10 @@ struct line {
     float y1;
     float y2;
 } line;
-/* REPLACE ME */
+// DATA STUFF END
 
-data * 
+
+// STUB STUFF
 
 #define _sort2(a,b)\
     if (a>b) {float __tmp = a;a = b; b = __tmp;}
@@ -37,18 +45,20 @@ int draw(){
     int my = matrix_gety();
     for (int x = 0; x < mx;x++){
         for (int y = 0; y < my;y++){
-            for (line l = lines/*TODO:LINES*/){
-                float x1 = l.x1;
-                float x2 = l.x2;
-                float y1 = l.y1;
-                float y2 = l.y2;
-                _sort2(x1,x2);
-                _sort2(y1,y2);
-                float x0 = (float) x;
-                float y0 = (float) y;
-                if ((x1 < x0 && x0 < x2)||(y1 < y0 && y0 < y2)){ // x or y relevant
-                    if ((x1 + (y0 - y1) * (x2-x1)/(y2-y1)) > x0) { // line cross above
+            for (struct polygon * poly = polys;;){
+                for (struct point * pt = poly->points;;){
+                    float x1 = pt->x;
+                    float x2 = pt->x;
+                    float y1 = pt->y;
+                    float y2 = pt->y;
+                    _sort2(x1,x2);
+                    _sort2(y1,y2);
+                    float x0 = (float) x;
+                    float y0 = (float) y;
+                    if ((x1 < x0 && x0 < x2)||(y1 < y0 && y0 < y2)){ // x or y relevant
+                        if ((x1 + (y0 - y1) * (x2-x1)/(y2-y1)) > x0) { // line cross above
 
+                        }
                     }
                 }
             }
