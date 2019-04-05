640f573f0c30ffab93125339a2b74d12c83f3f5a
cyriax <brast@cs.uni-frankfurt.de>
2019-04-05 02:06:55 +0200

    svg-util: stub improvements

--- svg-util/stub.c
+++ svg-util/stub.c
@@ -3,47 +3,46 @@
 int matrix_getx(){return 0;}
 int matrix_gety(){return 0;}
 
-typedef int RGB;
-void matrix_set(int x, int y, RGB color) {};
+//void matrix_set(int x, int y, RGB color) {};
 // MOCKUP STUFF END
 
-
 // DATA STUFF BEGIN
-struct point {
-    float x;
-    float y;
-} point;
-
 #define FIX_THIS 3
 struct polygon{
-    struct point points[3];
     int numpoints;
-    RGB color;
-    float min_x;
-    float max_x;
-    float min_y;
-    float max_y;
+    int startpoint;
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
 } polygon;
 
-struct line {
-    float x1;
-    float x2;
-    float y1;
-    float y2;
-} line;
 // DATA STUFF END
 
-// manually written triangle.
-int numpolys = 1;
-struct polygon polys[1] = {{
-								 .numpoints = 3,
-								 .points = {
-														{0.5f, 0.3f},
-														{0.8f, 0.8f},
-														{0.3f, 0.8f},
-								 },
-	}};
-
+// manually written triangles.
+#define NUMPOINTS 6
+float pointcloud[NUMPOINTS*2] = {
+    // x, y
+    0.5f, 0.3f,
+    0.8f, 0.8f,
+	0.3f, 0.8f,
+    0.11f, 0.3f,
+    0.8f, 0.8f,
+	0.3f, 0.8f
+};
+#define NUMPOLYS 2
+struct polygon polys[2] = {{
+	.numpoints = 3,
+    .startpoint = 0,
+    .red = 255,
+    .green = 0,
+    .blue = 0,
+	},{
+	.numpoints = 3,
+    .startpoint = 3,
+    .red = 0,
+    .green = 0,
+    .blue = 255,
+    }};
 // STUB STUFF
 
 #define _sort2(a,b)\
@@ -54,17 +53,17 @@ int draw(){
     int my = matrix_gety();
     for (int x = 0; x < mx;x++){
         for (int y = 0; y < my;y++){
-					float x0 = (float) x;
-					float y0 = (float) y;
-
-            for (struct polygon poly = polys[0];;){
-							int counter = 0;
-							// TODO: line iteration
-							for (struct point * pt = poly.points;;){
-                    float x1 = pt->x;
-                    float x2 = pt->x;
-                    float y1 = pt->y;
-                    float y2 = pt->y;
+            float x0 = (float) x;
+            float y0 = (float) y;
+            for (int i_poly = 0; i_poly < NUMPOLYS; i_poly++){
+                struct polygon poly = polys[i_poly];
+                int counter = 0;
+                // TODO: line iteration
+                for (int i_point = poly.startpoint;i_point < poly.startpoint + poly.numpoints;i_point++){
+                    float x1 = pointcloud[i_point];
+                    float y1 = pointcloud[i_point];
+                    float x2 = pointcloud[i_point];
+                    float y2 = pointcloud[i_point];
                     // TODO: line iteration
                     _sort2(x1,x2);
                     _sort2(y1,y2);
@@ -75,7 +74,7 @@ int draw(){
                     }
                 }
                 if (counter&1){
-                    matrix_set(x,y,poly->color);
+                    //matrix_set(x,y,RGB(poly.red,poly.green,poly.blue));
                 }
             }
         }
